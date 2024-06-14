8ee8c926383728da547df60d6aa1f7aebd05494c
Davies Evan <evandavies739@gmail.com>
2024-06-14 13:33:28 +0300

    EDAMAM Api configuration

--- backend/config.py
+++ backend/config.py
@@ -9,3 +9,6 @@ class ApplicationConfiguration:
     JWT_SECRET_KEY = os.getenv('APP_SECRET_KEY')
     JWT_ACCESS_TOKEN_EXPIRES = 900 #15 minutes
     JWT_REFRESH_TOKEN_EXPIRES = 86400 #1 day
+    EDAMAM_API_URL = "https://api.edamam.com/search"
+    EDAMAM_APP_ID = os.getenv("RECIPE_APP_ID")
+    EDAMAM_API_KEY = os.getenv("RECIPE_API")
