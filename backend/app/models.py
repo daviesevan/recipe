190a020766b71effab9d39c3ad3d0bad8748411d
Davies Evan <evandavies739@gmail.com>
2024-06-16 20:35:41 +0300

    isAdmin field

--- backend/app/models.py
+++ backend/app/models.py
@@ -28,6 +28,7 @@ class Admin(db.Model):
     fullname = db.Column(db.String(150), nullable=False)
     password = db.Column(db.String(325), nullable=False)
     isEmailVerified = db.Column(db.Boolean, default=False)
+    isAdmin = db.Column(db.Boolean, default=False)
     reset_token = db.Column(db.String(100), nullable=True)
     reset_token_expiration = db.Column(db.DateTime, nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.now)
